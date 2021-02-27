ce1def79f53310d92607b2b037218e5802df04a9
JianGuanTHU <j-guan19@mails.tsinghua.edu.cn>
2021-02-27 23:17:51 +0800

    fix class_lib

--- judger_class_lib.py
+++ judger_class_lib.py
@@ -1,5 +1,5 @@
 import os
-import io
+
 RS_AC = 0
 RS_JGF = 7
 RS_MLE = 3
@@ -32,7 +32,7 @@ RL_JUDGER_DEFAULT = RunLimit(600, 1024, 128)
 RL_CHECKER_DEFAULT = RunLimit(5, 256, 64)
 RL_VALIDATOR_DEFAULT = RunLimit(5, 256, 64)
 RL_MARKER_DEFAULT = RunLimit(5, 256, 64)
-RL_COMPILER_DEFAULT = RunLimit(60, 512, 64)
+RL_COMPILER_DEFAULT = RunLimit(15, 512, 64)
 
 class CustomTestInfo:
 	def __init__(self, ust=0, usm=0, info="", exp="", out=""):
@@ -103,7 +103,7 @@ def numbers_to_strings(argument):
 
 def info_str(id):
 	switcher = { \
-		RS_MLE: "Memory Limit Exceeded", \
+		RS_MLE: "zero", \
 		RS_TLE: "Time Limit Exceeded", \
 		RS_OLE: "Output Limit Exceeded", \
 		RS_RE: "Runtime Error", \
@@ -121,114 +121,71 @@ def run_program(main_path, result_file_name, input_file_name, output_file_name, 
 	para_list = para_list or []
 	readable = readable or []
 	#limit : RunLimit
-	# command = " ".join([main_path + "/run/run_program", \
-	# 					">" + escapeshellarg(result_file_name), \
-	# 					"--in=" + escapeshellarg(input_file_name), \
-	# 					"--out=" + escapeshellarg(output_file_name), \
-	# 					"--err=" + escapeshellarg(error_file_name), \
-	# 					"--tl=" + str(limit.time), \
-	# 					"--ml=" + str(limit.memory), \
-	# 					"--ol=" + str(limit.output), \
-	# 					])
-	# command += " --type=" + str(type) if (type) else ""
-	# command += " --work-path=" + work_path if (work_path) else ""
-	# command += " " +  " ".join([" --add-readable=" + each for each in readable])
-	# command += " " + " ".join([para for para in para_list])
-	# command += (" " + raw_para) if raw_para else ""
-	command = "cd %s" % (work_path)
-	command += " && %s" % (" ".join([para for para in para_list])) if len(para_list) != 0 else ""
-	command += (" && " + raw_para) if raw_para else ""
-	command += " <" + escapeshellarg(input_file_name)
-	command += " >" + escapeshellarg(output_file_name)
-	command += " 2>" + escapeshellarg(error_file_name)
-	command += " && cd %s" % main_path
-	print("command is : ", command)
+	command = " ".join([main_path + "/run/run_program", \
+						">" + escapeshellarg(result_file_name), \
+						"--in=" + escapeshellarg(input_file_name), \
+						"--out=" + escapeshellarg(output_file_name), \
+						"--err=" + escapeshellarg(error_file_name), \
+						"--tl=" + str(limit.time), \
+						"--ml=" + str(limit.memory), \
+						"--ol=" + str(limit.output), \
+						])
+	command += " --type=" + str(type) if (type) else ""
+	command += " --work-path=" + work_path if (work_path) else ""
+	command += " " +  " ".join([" --add-readable=" + each for each in readable])
+	command += " " + " ".join([para for para in para_list])
+	command += (" " + raw_para) if raw_para else ""
+	print "command is : ", command
 	try:
-		exit_code_ = os.system(command)
-		value, exit_code = (exit_code_)>>8, exit_code_&0xff
-		#print io.open(result_file_name,'r').readline()
-		# data_raw = '\n'.join(io.open(result_file_name,'r').readline().split(' '))
-		data = "\n".join(io.open(error_file_name,'r').readlines()).strip()
-		# result = RunResult(int(data[0]), int(data[1]), int(data[2]), int(data[3]))
-		print("value:", value, "exit_code:", exit_code, "data", data)
-		if value != 0:
-			result = RunResult(type=RS_RE, exit_code=exit_code)
-		else:
-			result = RunResult(exit_code=exit_code)
+		os.system(command)
+		#print open(result_file_name,'r').readline()
+		data_raw = '\n'.join(open(result_file_name,'r').readline().split(' '))
+		data = open(result_file_name,'r').readline().strip().split(' ')
+		print("data", data)
+		result = RunResult(int(data[0]), int(data[1]), int(data[2]), int(data[3]))
 		return result
 	except:
-		import traceback; traceback.print_exc();
 		return RunResult_failed_result()
-
-# def run_program(main_path, result_file_name, input_file_name, output_file_name, error_file_name, \
-# 		limit, para_list=None, type=None, work_path=None, readable=None, raw_para=None):
-
-# 	para_list = para_list or []
-# 	readable = readable or []
-# 	#limit : RunLimit
-# 	command = " ".join([main_path + "/run/run_program", \
-# 						">" + escapeshellarg(result_file_name), \
-# 						"--in=" + escapeshellarg(input_file_name), \
-# 						"--out=" + escapeshellarg(output_file_name), \
-# 						"--err=" + escapeshellarg(error_file_name), \
-# 						"--tl=" + str(limit.time), \
-# 						"--ml=" + str(limit.memory), \
-# 						"--ol=" + str(limit.output), \
-# 						])
-# 	command += " --type=" + str(type) if (type) else ""
-# 	command += " --work-path=" + work_path if (work_path) else ""
-# 	command += " " +  " ".join([" --add-readable=" + each for each in readable])
-# 	command += " " + " ".join([para for para in para_list])
-# 	command += (" " + raw_para) if raw_para else ""
-# 	print("command is : ", command.encode('utf-8'))
-# 	try:
-# 		os.system(command)
-# 		#print io.open(result_file_name,'r').readline()
-# 		data_raw = '\n'.join(io.open(result_file_name, 'r', encoding='utf-8').readline().split(' '))
-# 		data = io.open(result_file_name, 'r', encoding='utf-8').readline().strip().split(' ')
-# 		print("data", data)
-# 		result = RunResult(int(data[0]), int(data[1]), int(data[2]), int(data[3]))
-# 		return result
-# 	except:
-# 		import traceback; traceback.print_exc();
-# 		return RunResult_failed_result()
 
 def file_preview(input_file_name, range=100):
 	try:
-		str = "".join(io.open(input_file_name, 'r', encoding='utf-8').readlines())
+		str = "".join(open(input_file_name, 'r').readlines())
 		if len(str) > range * 4:
 			return str[:range * 4] + "..."
 		else:
 			return str
 	except:
-		import traceback; traceback.print_exc();
-		return "no such file:" + input_file_name
+		return "no such file:"+input_file_name
 
 def file_hide_token(file_name, token):
 	# examine token
 	try:
-		f = io.open(file_name, "r", encoding='utf-8')
+		f = open(file_name, "r")
 		data = f.read()
 		f.close()
 		if data[:len(token)] != token:
 			raise Exception
-		f = io.open(file_name, "w", encoding='utf-8')
+		f = open(file_name, "w")
 		f.write(data[len(token):])
 		f.close()
 	except:
-		import traceback; traceback.print_exc();
-		f = io.open(file_name, "w", encoding='utf-8')
+		f = open(file_name, "w")
 		f.write("Unauthorized output\n")
 		f.close()
 
 def conf_run_limit(pre, index, val, config):
-	def init_limit(key, default):
-		return config.config[key] if key in config.config else default
-	if pre == "":
-		pre = "_"
-	return RunLimit(time=init_limit(pre+"time_limit_" + str(index), val.time), \
-					memory=init_limit(pre+"memory_limit_" + str(index), val.memory), \
-					output=init_limit(pre+"output_limit_" + str(index), val.output) \
+	def init_limit(key, index, default):
+		if key + "_" + index in config.config:
+			return config.config[key + "_" + index]
+		if key in config.config:
+			return config.config[key]
+		return default
+
+	if pre != "":
+		pre += "_"
+	return RunLimit(time=init_limit(pre+"time_limit", str(index), val.time), \
+					memory=init_limit(pre+"memory_limit", str(index), val.memory), \
+					output=init_limit(pre+"output_limit", str(index), val.output) \
 					)
 
 def htmlspecialchars(string=""):
@@ -243,16 +200,14 @@ def check_file_exist(work_path, result_path, assertfile=[], banfile=[]):
 	os.system("cd %s; ls > %s" % (escapeshellarg(work_path), escapeshellarg(result_path + "/filelist.txt")))
 	assertfile = set(assertfile)
 	banfile = set(banfile)
-	try:
-		for tmp in io.open(result_path + "/filelist.txt", encoding='utf-8'):
-			tmp = tmp.strip()
-			if tmp in banfile:
-				return False, "found unexpcted file '" + tmp + "' in your dir"
-			if tmp in assertfile:
-				assertfile.remove(tmp)
-		if not len(assertfile):
-			return True, "ok"
-		else:
-			return False, "didn't find expected file '" + str(list(assertfile)[0]) + "' in your dir"
-	except Exception:
-		return False, "check filename failed. Please make sure there is no Chinese character in your file name."
+
+	for tmp in open(result_path + "/filelist.txt"):
+		tmp = tmp.strip()
+		if tmp in banfile:
+			return False, "found unexpcted file '" + tmp + "' in your dir"
+		if tmp in assertfile:
+			assertfile.remove(tmp)
+	if not len(assertfile):
+		return True, "ok"
+	else:
+		return False, "didn't find expected file '" + str(list(assertfile)[0]) + "' in your dir"
